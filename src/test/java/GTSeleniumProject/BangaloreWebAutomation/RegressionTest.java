129e9f7e21a7963dab73520c9aaefe7cf5fd3752
Sathishmiami <kumar.test2019.com>
2020-08-23 11:21:56 -0400

    BangaloreProect added

---
+++ src/test/java/GTSeleniumProject/BangaloreWebAutomation/RegressionTest.java
@@ -0,0 +1,38 @@
+package GTSeleniumProject.BangaloreWebAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+
+
+	
+	@Test
+	void browserOpen() {  
+	System.out.println("Regrssion Browser launching");
+	
+	}
+	
+	@Test
+	void ApplicationLanching() {  
+		System.out.println("Regrssion Application launching");
+
+	}
+	
+	@Test
+	void LoginPage() {  
+		System.out.println("RegrssionApplication login page");
+	
+	}
+	
+	@Test
+	void HomePage() {  
+		System.out.println("Regrssion Application login page");
+	
+	}
+
+
+
+	
+}
---
+++ src/test/java/GTSeleniumProject/BangaloreWebAutomation/SmokeTest.java
@@ -0,0 +1,32 @@
+package GTSeleniumProject.BangaloreWebAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+
+	
+	@Test
+	void browserOpen() {  
+	System.out.println("Browser launching");
+	
+	}
+	
+	@Test
+	void ApplicationLanching() {  
+		System.out.println("Application launching");
+	
+	}
+	
+	@Test
+	void LoginPage() {  
+		System.out.println("Application login page");
+	
+	}
+
+
+
+	
+}
